da453649c49e1ef0807197809b7d64c71e16e227
magieup <magieup@univ-grenoble-alpes.fr>
2026-02-10 14:34:48 +0100

    add history loading

--- src/main/java/server/api/message/DefaultMessageProvider.java
+++ src/main/java/server/api/message/DefaultMessageProvider.java
@@ -7,6 +7,7 @@ import shared.api.message.MessageService;
 import shared.api.message.SpaceSubscriber;
 
 import java.rmi.RemoteException;
+import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -16,6 +17,8 @@ import java.util.logging.Level;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 
 public class DefaultMessageProvider implements MessageService {
@@ -36,6 +39,23 @@ public class DefaultMessageProvider implements MessageService {
     @Override
     public void subscribe(final Identity identity, final SpaceSubscriber subscriber) {
         this.subscriberMap.put(identity.username(), subscriber);
+
+        //Send history message to subscriber
+        try(BufferedReader br = new BufferedReader(new FileReader(PATH_HISTORY_FILE))){
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                //Parse to create Message
+                String[] parse = line.split(":", 3);
+
+                if(parse.length == 3){
+                    subscriber.onMessage(new Message(parse[0], parse[1], Instant.parse(parse[2])));
+                }
+
+            }
+        }catch(IOException e){
+            System.err.println("[ERR] Error read history : "+e);
+        }
     }
 
     @Override
@@ -52,7 +72,7 @@ public class DefaultMessageProvider implements MessageService {
         synchronized (mutex){
             try(FileWriter writer = new FileWriter(PATH_HISTORY_FILE);){
                 //Formated message
-                String m = "%s at %s : %s\n".formatted(message.username(), message.date(), message.message());
+                String m = "%s:%s:%s\n".formatted(message.username(), message.date(), message.message());
 
                 writer.write(m);
 
